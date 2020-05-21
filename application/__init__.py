d5b5cc136c4d34327546e6146752f3025e1b2295
kodezen <kodezen@protonmail.com>
2020-05-21 14:15:41 -0400

    Added CORS support.

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_socketio import SocketIO
 
 socketio = SocketIO(cors_allowed_origins="*")
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
