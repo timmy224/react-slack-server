42a46efb37af459ea5bbc263e8a4e9ab2fbf0da8
kodezen <kodezen@protonmail.com>
2020-06-01 18:05:25 -0400

    Updated imports to use __all__

--- application/main/__init__.py
+++ application/main/__init__.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 main = Blueprint("main", __name__)
 
 from . import events
-from .routes import user, message, channel
+from .routes import *
 
 
--- application/main/routes/__init__.py
+++ application/main/routes/__init__.py
@@ -3,6 +3,8 @@ from . import message
 from . import channel
 from .. import main
 
+__all__ = ["user", "message", "channel"]
+
 @main.route("/")
 def index():
     return "<h1>Hello World!</h1>"
