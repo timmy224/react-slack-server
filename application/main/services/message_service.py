90892fd71a5162e573c602a9a6c734aca855ac40
Timmy <Timmy224@gmail.com>
2020-06-06 12:03:11 -0400

    add named import for Message class vs. Message model; add store_private_message()

--- application/main/services/message_service.py
+++ application/main/services/message_service.py
@@ -1,6 +1,10 @@
 from datetime import datetime
 from .channel_service import get_ind_channel, add_message_channel
-from .message_class import Message
+from .message_class import Message as Message_class
+from ... import db
+from ...models.User import User, user_schema
+from ...models.Message import Message as Message_model, message_schema
+
 # class Message():
 #     def __init__(self, sender, time_sent, content, channel_id):
 #         self.sender = sender
@@ -21,7 +25,7 @@ from .message_class import Message
         #messages.append(message)
 
 def on_send_message(clientMessage):
-    message = Message(clientMessage["sender"], 
+    message = Message_class(clientMessage["sender"], 
                       clientMessage["time_sent"], 
                       clientMessage["content"],
                       clientMessage["channel_id"]
@@ -34,5 +38,25 @@ def get_recent_messages(channel_id):
     
     print(curr_channel.messages[-25:])
     return curr_channel.messages[-25:]
+
+def store_private_message(clientMessage):
+    """
+    Use the User model to query for the person sending and the person receiving.
+    Create a new Message model and add the sender and receiver as properties on 
+    the Message object. Then add the Message object to the database 
+    """
+    sender_id = User.query(User.user_id).filter_by(username=clientMessage["sender"])
+    content = clientMessage['content']
+    sent_dt = datetime.strptime(clientMessage["sent_dt"],  "%m/%d/%Y %I:%M%p")
+
+    sender = clientMessage['Sender']
+    receiver = User.query(User.user_id).filter_by(username=clientMessage["receiver"])
+    message = Message_model(sender_id, sent_dt, content)
+
+    message.sender = sender
+    message.receiver = receiver
+    
+    db.session.add(message)
+    db.session.commit()
 
 # add_dummy_messages()
