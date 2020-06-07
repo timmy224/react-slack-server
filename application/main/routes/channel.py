6f9352dd9c2e0c164cc4aa4afab6d321e69cd09e
Timmy <Timmy224@gmail.com>
2020-06-06 20:05:47 -0400

    modify fetchChannels call to get from channels col channel_id

--- application/main/routes/channel.py
+++ application/main/routes/channel.py
@@ -15,10 +15,17 @@ def get_channels():
     Path: /channels/
     Response Body: "channels"
     """
-    channel_ids = channel_service.get_channel_ids()
-    channels_json = json.dumps(channel_ids) 
+    # channel_ids = channel_service.get_channel_ids()
+    # channels_json = json.dumps(channel_ids) 
+    # response = {}
+    # response["channels"] = channels_json 
+    # print(response)
+    # {'channels': '[1, 2, 3, 4]'}
+    #channel = Channel.query.with_entities(Channel.channel_id).all()
+    channels = [r.channel_id for r in db.session.query(Channel.channel_id)]
     response = {}
-    response["channels"] = channels_json 
+    response["channels"] = f'{channels}'
+    print(response)
     return response
 
 ### DATABASE ROUTES ###
