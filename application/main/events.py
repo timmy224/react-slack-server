d62dc1eaa487ca95937e1cb58aaf59601a92373f
Luis Zavala <lz24069@gmail.com>
2020-06-10 16:46:47 -0400

    remove unneccesary changes

--- application/main/events.py
+++ application/main/events.py
@@ -9,8 +9,6 @@ import json
 @socketio.on("connect")
 def on_connect():
     username = request.args.get("username")
-    user = db.session.query(User.user_id).filter_by(username={username})
-    channels_joined = user.channels
     print(f"Client connected! username: {username}")
     # All clients are assigned a personal room by Flask SocketIO when they connect, named with the session ID of the connection. We want to store this so that we can relay messages to individual clients in the future using send/emit(..., room=room)
     room = request.sid
@@ -20,20 +18,7 @@ def on_connect():
     emit("message-catchup", recent_messages)
     # Broadcast to all other clients that a new client connected
     emit("user-joined-chat", {"username": username}, broadcast=True, include_self=False)
-'''
-**on socket connection**
 
-When a client connects, use the user_id to look up the channels to which 
-that user belongs using a SQLAlchemy query. If you query using the User model, 
-the channels will be available as a property on the user object. 
-
-Now that you can get at that channels, we're going to make the user join one 
-socket-io room for each channel, where the room is the channel_id. 
-
-join_room(channel1.id)
-join_room(channel2.id)
-join_room(channel3.id)
-'''
 @socketio.on("send-message")
 def on_send_message(clientMessage):
     print("Client sent message")
