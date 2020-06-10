fd78e462d08e4dbcb19e7451f58b137b75c99044
Sleyter De La Cruz <Sleyter2616@gmail.com>
2020-06-09 20:02:34 -0400

    Socket private message

--- application/main/events.py
+++ application/main/events.py
@@ -1,10 +1,12 @@
 import json
 from flask import request
-from flask_socketio import emit, join_room, leave_room # didn't add leave_room anywhere yet
+# didn't add leave_room anywhere yet
+from flask_socketio import emit, join_room, leave_room
 from .. import socketio
 from .services import client_service
 from .services import message_service
 import json
+
 
 @socketio.on("connect")
 def on_connect():
@@ -13,11 +15,15 @@ def on_connect():
     # All clients are assigned a personal room by Flask SocketIO when they connect, named with the session ID of the connection. We want to store this so that we can relay messages to individual clients in the future using send/emit(..., room=room)
     room = request.sid
     client_service.on_client_connected(username, room)
-    recent_messages = message_service.get_recent_messages(1) # set default channel here, but removed client socket listener for message-catchup
-    recent_messages = json.dumps([message.__dict__ for message in recent_messages])
+    # set default channel here, but removed client socket listener for message-catchup
+    recent_messages = message_service.get_recent_messages(1)
+    recent_messages = json.dumps(
+        [message.__dict__ for message in recent_messages])
     emit("message-catchup", recent_messages)
     # Broadcast to all other clients that a new client connected
-    emit("user-joined-chat", {"username": username}, broadcast=True, include_self=False)
+    emit("user-joined-chat", {"username": username},
+         broadcast=True, include_self=False)
+
 
 @socketio.on("send-message")
 def on_send_message(clientMessage):
@@ -28,10 +34,19 @@ def on_send_message(clientMessage):
     if clientMessage['type'] == "channel":
         message_service.store_channel_message(clientMessage)
         channel_room = clientMessage['channel_id']
-        emit("message-received", clientMessage, room=channel_room, broadcast=True, include_self=True)
-    else:
-        # insert private message logic here
+        emit("message-received", clientMessage,
+             room=channel_room, broadcast=True, include_self=True)
+
+        # where the socket handles the private messages for storing and sending.
+    elif clientMessage['type'] == "private":
+        message_service.store_private_message(clientMessage)
+        receiver_room = clientMessage['reciever']
+        emit("message-recieved", clientMessage,
+             room=receiver_room, broadcast=True, include_self=True)
         pass
+
+    print("After send")
+
 
 @socketio.on("disconnect")
 def on_disconnect():
