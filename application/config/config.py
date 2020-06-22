28ed73b982bdec62790e94349dcd5ad579b34116
kodezen <kodezen@protonmail.com>
2020-06-22 09:52:21 -0400

    Added missing self.

--- application/config/config.py
+++ application/config/config.py
@@ -5,11 +5,11 @@ class Config:
 
 class DevelopmentConfig(Config):
     def __init__(self):
-        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL_DEV")
+        self.SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL_DEV")
  
 class ProductionConfig(Config):
     def __init__(self):
-        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL_PROD")
+        self.SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL_PROD")
 
 def getConfig():
     return DevelopmentConfig() if os.getenv("MODE") == "development" else ProductionConfig()
