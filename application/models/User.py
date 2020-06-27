404a0f2eac449d6d76183119f494baf2430f0f5e
Sleyter De La Cruz <Sleyter2616@gmail.com>
2020-06-27 14:06:55 -0400

    added the hashing of the password to the user model, so that the DB stores the hashed password and not the actual password.

--- application/models/User.py
+++ application/models/User.py
@@ -1,9 +1,18 @@
 from .. import db, ma
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = "users"
     user_id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String())
+    username = db.Column(db.String(),index=True, unique=True)
+    password_hash= db.Column(db.String())
+
+    def set_password (self, password):
+        self.password_hash= generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __init__(self, username):
         self.username = username 
