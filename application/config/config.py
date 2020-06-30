f19ae5bcc0f7e6b9ba77bc526f7268a8f580bcd2
kodezen <kodezen@protonmail.com>
2020-06-30 12:44:44 -0400

    Added httponly configuration for flask and flask-login cookies.

--- application/config/config.py
+++ application/config/config.py
@@ -3,8 +3,10 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = None # SQLAlchemy configuration setting
     SECURE_COOKIES = None # Cookies manually created by us configuration setting
-    SESSION_COOKIE_SECURE = None # Flask-Login session cookie configuration setting
+    SESSION_COOKIE_SECURE = None # Flask session cookie configuration setting
     REMEMBER_COOKIE_SECURE = None # Flask-Login remember cookie configuration setting
+    SESSION_COOKIE_HTTPONLY = None # Flask session cookie configuration setting
+    REMEMBER_COOKIE_HTTPONLY = None # Flask-Login remember cookie configuration setting
 
 class DevelopmentConfig(Config):
     def __init__(self):
@@ -12,6 +14,8 @@ class DevelopmentConfig(Config):
         self.SECURE_COOKIES = False
         self.SESSION_COOKIE_SECURE = False 
         self.REMEMBER_COOKIE_SECURE = False
+        self.SESSION_COOKIE_HTTPONLY = True
+        self.REMEMBER_COOKIE_HTTPONLY = True
  
 class ProductionConfig(Config):
     def __init__(self):
@@ -19,6 +23,8 @@ class ProductionConfig(Config):
         self.SECURE_COOKIES = True
         self.SESSION_COOKIE_SECURE = True 
         self.REMEMBER_COOKIE_SECURE = True
+        self.SESSION_COOKIE_HTTPONLY = True
+        self.REMEMBER_COOKIE_HTTPONLY = True
 
 def getConfig():
     return DevelopmentConfig() if os.getenv("MODE") == "development" else ProductionConfig()
