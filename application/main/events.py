b754a6a6f08053e3f3db2055af827c0b09a636ce
kodezen <kodezen@protonmail.com>
2020-07-20 16:31:08 -0400

    Got private messages working.

--- application/main/events.py
+++ application/main/events.py
@@ -55,7 +55,7 @@ def on_send_message(clientMessage):
         receiver_client = client_service.clients.get(receiver_username)
         if receiver_client is not None:
             receiver_room = receiver_client.room
-            emit("message-recieved", clientMessage, room=receiver_room, include_self=True)
+            emit("message-received", clientMessage, room=receiver_room, include_self=True)
         
 @socketio.on("join-channel")
 def on_join_channel(channel_id):
