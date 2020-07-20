1cff1752c28f33f1af779645cb91025a973ee170
Sleyter De La Cruz <Sleyter2616@gmail.com>
2020-07-20 13:19:11 -0400

    corrected logic for checking if client object exists for reciever

--- application/main/events.py
+++ application/main/events.py
@@ -54,10 +54,12 @@ def on_send_message(clientMessage):
         emit("message-received", clientMessage, room=channel_room, broadcast=True, include_self=True)
     elif clientMessage['type'] == "private":
         message_service.store_private_message(clientMessage)
-        receiver_room = clientMessage.get('receiver')
-        if receiver_room is not None:S
+        receiver_username = clientMessage['receiver']
+        receiver_client = client_service.clients.get(receiver_username)
+        if receiver_client is not None:
+            receiver_room = receiver_client.room
             emit("message-recieved", clientMessage, room=receiver_room, broadcast=True, include_self=True)
-       
+        
 @socketio.on("join-channel")
 def on_join_channel(channel_id):
     print("join_channel:", channel_id)
