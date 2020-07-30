5f35a4a6e86aa83663c8879f6859a57f85bbce89
Luis <lz24069@gmail.com>
2020-07-30 16:14:54 -0400

    configured user.py in routes

--- application/main/routes/user.py
+++ application/main/routes/user.py
@@ -4,12 +4,11 @@ from flask_wtf.csrf import generate_csrf
 import json
 from .. import main
 from ... import db
-from ..services import client_service
-from ...models.User import User, user_schema
+from ...models.User import User
 from ...models.Channel import Channel
 from sqlalchemy.orm.exc import NoResultFound
 
-@main.route("/register", methods={"POST"})
+@main.route("/register", methods=["POST"])
 def register_user():
     response = {}
     data = request.json
@@ -41,17 +40,9 @@ def register_user():
     else:
         response["ERROR"] = "Username is taken"
         return jsonify(response)
-            
-### DATABASE ROUTES ###
 
 @main.route("/usernames", methods=["GET"])
 def get_users():
-    """
-    [GET] - Grabs the usernames from the DB and returns a list of usernames as a JSON response
-    Path: /users
-    Response Body: "usernames"    
-    DB tables: "users"
-    """
     response = {}
     results = db.session.query(User.username).all()
     usernames = [result[0] for result in results]
@@ -98,40 +89,3 @@ def login():
 def protected_route():
     print("Printing current user in protected route: ", current_user.username)
     return {}
-
-# @main.route("/user/", methods=["GET", "POST"])
-# def user():
-#     """
-#     [GET] - Grabs the user from the DB and returns it as a JSON response
-#     Path: /user/?user_id={user_id}
-#     Response Body: "user"
-    
-#     [POST] - Inserts a user into the DB using JSON passed in as request body
-#     Path: /user
-#     Request Body: "username"
-#     Response Body: "successful"
-
-#     DB tables: "users"
-#     """
-#     if request.method == "GET":
-#         user_id = request.args.get("user_id", None)
-#         response = {}
-#         if user_id is None:
-#             response["ERROR"] = "Missing user_id in route"
-#             return jsonify(response)
-#         user = User.query.filter_by(user_id=user_id).one()
-
-#         user_json = user_schema.dump(user)
-#         response["user"] = user_json
-#         return response
-#     elif request.method == "POST":
-#         data = request.json
-#         user = User(data["username"])
-
-#         db.session.add(user)
-#         db.session.commit()
-
-#         print("SUCCESS: user inserted into db")
-#         response = {}
-#         response["successful"] = True
-#         return jsonify(response)
