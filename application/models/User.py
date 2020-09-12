3643d9363d7fe6830805b57b3dd838cb77739dd3
kodezen <kodezen@protonmail.com>
2020-09-12 14:58:10 -0400

    Removed autoincrement I added unnecessarily

--- application/models/User.py
+++ application/models/User.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(UserMixin, db.Model):
     __tablename__ = "users"
-    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), index=True, unique=True)
     password_hash = db.Column(db.String())
 
