9c9bddfc81217d1e5a42505253fbcba7e615b194
kodezen <kodezen@protonmail.com>
2020-10-16 09:39:56 -0400

    Created roles constants file containing the role names used in our app

---
+++ application/constants/roles.py
@@ -0,0 +1,17 @@
+# ORG
+class OrgRoles:
+    TADPOLE = "org_tadpole"
+    AMBASSADOR = "org_ambassador"
+    MODERATOR = "org_moderator"
+    ADMIN = "org_admin"
+
+# CHANNEL
+class ChannelRoles:
+    TADPOLE = "channel_tadpole"
+    AMBASSADOR = "channel_ambassador"
+    MODERATOR = "channel_moderator"
+    ADMIN = "channel_admin"
+
+org = OrgRoles()
+channel = ChannelRoles()    
+
