5314f05c3dd248df9819fc1c14d3c9d2eac89811
kodezen <kodezen@protonmail.com>
2020-12-05 20:28:35 -0500

    - Updated disconnect logic. I noticed that sometimes the disconnecting socket room was not associated with a user when force quitting the server (leading to username being None). Now we only try to get_user when username is not None, which will prevent sql alchemy error (expected username to exist in db because of .one()) - Created an event_service method for sending "permissions-changed" event, was missing when creating an org (now notifying the org creator that his/her permissions have been updated) - Created an event_service method for sending "added-to_org" event - Updated permission route to POST - Created permission_service methods: get_org_member_perms and get_channel_member_perms to clean up permissions route

--- application/main/services/client_service.py
+++ application/main/services/client_service.py
@@ -12,7 +12,6 @@ def add_client(username, room):
 def remove_client_by_room(room):
     for username, client in list(clients.items()):
         if client.room == room:
-            print(f"Removed {username} from clients")
             del clients[username]
             break
 
